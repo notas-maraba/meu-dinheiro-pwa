a5f58cec3bf0cc94c306e271d4c74858ee845b06
GP Gestor de Processos <gp.gestordeprocessos@gmail.com>
2026-09-15 13:47:41 -0300

    Usa o ícone arredondado no aplicativo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='meu-dinheiro-icon-v12';
+const CACHE='meu-dinheiro-icon-rounded-v13';
 const FILES=['./','./index.html','./style.css','./cloud.css','./zoom.js','./cloud.js','./firebase-config.js','./app.js?v=10','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('meu-dinheiro-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
@@ -10,6 +10,7 @@ self.addEventListener('fetch',e=>{
  }).catch(async()=>await caches.match(e.request)||(e.request.mode==='navigate'?await caches.match('./index.html'):Response.error())));
 });
 self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.openWindow('./#contas'))});
+
 
 
 
