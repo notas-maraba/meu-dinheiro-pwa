a8d98007e14ed9edf98ab3f97add2b95d78cad5b
notas-maraba <gp.gestordeprocessos@gmail.com>
2026-09-15 08:51:39 -0300

    Corrige proporções no celular e restaura layout do PC
    Aplica o modo móvel até 900 px, ocupa toda a largura disponível e mantém um layout amplo com barra lateral no computador.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='meu-dinheiro-mobile-v6';
+const CACHE='meu-dinheiro-responsive-v7';
 const FILES=['./','./index.html','./style.css','./cloud.css','./cloud.js','./firebase-config.js','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('meu-dinheiro-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
