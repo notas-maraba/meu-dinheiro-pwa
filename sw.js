b1d9edc5a7e04f60d4916d3816751ffb58a5e753
GP Gestor de Processos <gp.gestordeprocessos@gmail.com>
2026-09-15 13:52:33 -0300

    Força atualização visual no celular

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='meu-dinheiro-mobile-large-v14';
-const FILES=['./','./index.html','./style.css','./cloud.css','./zoom.js','./cloud.js','./firebase-config.js','./app.js?v=10','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon.svg'];
+const CACHE='meu-dinheiro-mobile-large-v15';
+const FILES=['./','./index.html','./style.css?v=14','./cloud.css?v=14','./zoom.js','./cloud.js','./firebase-config.js','./app.js?v=10','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('meu-dinheiro-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
@@ -10,6 +10,7 @@ self.addEventListener('fetch',e=>{
  }).catch(async()=>await caches.match(e.request)||(e.request.mode==='navigate'?await caches.match('./index.html'):Response.error())));
 });
 self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.openWindow('./#contas'))});
+
 
 
 
